3599f26cfdd846c50da29db38584de12fda768d7
Michael Hadley <michaelwesthadley@gmail.com>
2016-06-28 20:14:50 -0500

    Bring utilities from personal site

---
+++ src/js/utilities.js
@@ -0,0 +1,99 @@
+exports.default = function (val, defaultVal) {
+    return (val !== undefined) ? val : defaultVal;
+};
+
+// Untested
+// exports.defaultProperties = function defaultProperties (obj, props) {
+//     for (var prop in props) {
+//         if (props.hasOwnProperty(props, prop)) {
+//             var value = exports.defaultValue(props.value, props.default);
+//             obj[prop] = value;
+//         }
+//     }
+//     return obj;
+// };
+// 
+exports.timeIt = function (func) {
+    var start = performance.now();
+    func();
+    var end = performance.now();
+    return end - start;
+};
+
+exports.isInRect = function (x, y, rect) {
+    if (x >= rect.x && x <= (rect.x + rect.w) &&
+        y >= rect.y && y <= (rect.y + rect.h)) {
+        return true;
+    }
+    return false;
+};
+
+exports.randInt = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+exports.randArrayElement = function (array) {
+    var i = exports.randInt(0, array.length - 1);    
+    return array[i];
+};
+
+exports.map = function (num, min1, max1, min2, max2, options) {
+    var mapped = (num - min1) / (max1 - min1) * (max2 - min2) + min2;
+    if (!options) return mapped;
+    if (options.round && options.round === true) {
+        mapped = Math.round(mapped);
+    }
+    if (options.floor && options.floor === true) {
+        mapped = Math.floor(mapped);        
+    }
+    if (options.ceil && options.ceil === true) {
+        mapped = Math.ceil(mapped);        
+    }
+    if (options.clamp && options.clamp === true) {
+        mapped = Math.min(mapped, max2);
+        mapped = Math.max(mapped, min2);
+    }
+    return mapped;
+};
+
+exports.getQueryParameters = function () {
+    // Check for query string
+    qs = window.location.search;
+    if (qs.length <= 1) return {};
+    // Query string exists, parse it into a query object
+    qs = qs.substring(1); // Remove the "?" delimiter
+    var keyValPairs = qs.split("&");
+    var queryObject = {};
+    for (var i = 0; i < keyValPairs.length; i += 1) {
+        var keyVal = keyValPairs[i].split("=");
+        if (keyVal.length === 2) {
+            var key = decodeURIComponent(keyVal[0]);
+            var val = decodeURIComponent(keyVal[1]);
+            queryObject[key] = val;
+        }
+    }
+    return queryObject;
+};
+
+exports.createQueryString = function (queryObject) {
+    if (typeof queryObject !== "object") return "";
+    var keys = Object.keys(queryObject);
+    if (keys.length === 0) return "";
+    var queryString = "?";
+    for (var i = 0; i < keys.length; i += 1) {
+        var key = keys[i];
+        var val = queryObject[key];
+        queryString += encodeURIComponent(key) + "=" + encodeURIComponent(val);
+        if (i !== keys.length - 1) queryString += "&";
+    }
+    return queryString;
+};
+
+exports.wrapIndex = function (index, length) {
+    var wrappedIndex = (index % length); 
+    if (wrappedIndex < 0) {
+        // If negative, flip the index so that -1 becomes the last item in list 
+        wrappedIndex = length + wrappedIndex;
+    }
+    return wrappedIndex;
+};
